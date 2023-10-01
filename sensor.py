dcefe8f43056d981047b95767785db3c1479f8a6
Jonas <sparkofska@gmail.com>
2023-10-01 20:47:27 +0200

    Add stub sensor in case the app is not running on a raspberry pi

--- sensor.py
+++ sensor.py
@@ -1,11 +1,20 @@
 import time
-import board
-import adafruit_dht
+import random
+
+try:
+    import board
+    import adafruit_dht
+
+    IS_RASPI_BOARD = True
+except ImportError:
+    IS_RASPI_BOARD = False
+
 
 class SensorReadException(Exception):
     pass
 
-class ClimateSensor:
+
+class _ClimateSensorDHT22:
 
     def __init__(self):
         # Initial the dht device, with data pin connected to:
@@ -18,7 +27,7 @@ class ClimateSensor:
             try:
                 # Print the values to the serial port
                 temperature_c = self._dhtDevice.temperature
-                #temperature_f = temperature_c * (9 / 5) + 32
+                # temperature_f = temperature_c * (9 / 5) + 32
                 humidity = self._dhtDevice.humidity
 
                 return (temperature_c, humidity)
@@ -33,6 +42,20 @@ class ClimateSensor:
 
         raise SensorReadException(f'Unable to read sensor after {tries} tries')
 
+
+class _ClimateSensorRandomStub:
+    def __init__(self):
+        self._temperature = random.gauss(22.0, 5.0)
+        self._humidity = random.gauss(50.0, 15.0)
+
+    def read(self):
+        self._temperature = round(self._temperature + random.uniform(-2.0, +2.0), 1)
+        self._humidity = round(self._humidity + random.uniform(-5.0, +5.0), 1)
+
+        return (self._temperature, self._humidity)
+
+
+ClimateSensor = _ClimateSensorDHT22 if IS_RASPI_BOARD else _ClimateSensorRandomStub
 
 if __name__ == '__main__':
     sensor = ClimateSensor()
